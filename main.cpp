350ea896def59023809064063978b7c3dc55fe4f
Deepak <116195780+Deepak-Velmurugan@users.noreply.github.com>
2025-03-11 20:53:01 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins Pipeline\n";
+  cout << "I have successfully built and run this task-5 \n";
+  cout << "learnt pipelines !! :) \n";
+  return 0;
+}
